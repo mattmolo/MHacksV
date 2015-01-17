6f58e9e688100d91dfac7a5f22a445583489dcf0
Matt Molo <molo.matt@gmail.com>
2015-01-17 02:24:25 -0500

    Grid config works

--- js/main.js
+++ js/main.js
@@ -1,86 +1,21 @@
 function grid_init() {
 
-    var grid = new Array(0,0,0, 0,0,0, 0,0,0, 0,0,0, 0,0,0)
+    var xOrigin = 0;
+    var yOrigin = 120;
+    var baseSize = 360;
 
-	$.getJSON("grid.json", function(json) {
-	    console.log(json);
-	    $.each(json, function( key, val ) {
-            var works = false;
+    $.getJSON("grid.json", function(json) {
+        console.log(json);
+        $.each(json, function(name, grid) {
+            var left = xOrigin + ((grid.origin % 3) * baseSize);
+            var top = yOrigin + (Math.floor(grid.origin/3) * baseSize);
 
-            var heigth = val.height;
-            var width = val.width;
+            $('<div/>', {
+                id: name,
+                style: "left: " + left + "px; top: " + top + "px;",
+                class: "tile " + grid.width + " " + grid.height
+            }).appendTo('.main-grid');
 
-            //place in array to place the first block
-            var startPosition = 0;
-
-            for (var i = 0; 0 < 15; i++) {
-
-                //Something is there, so look at next place
-                if (grid[i] == 1) {
-                    continue;
-
-                //Nothing is there, so check surrounding spots
-                } else if (grid[i] == 0) {
-
-                    //Check if height is available
-                    var heightWorks = true;
-                    for (var j = 1; j <= heigth; j++) {
-
-                        //Computes the spot we're checking
-                        //Multiply be 3 to offset to the next row
-                        var idx = i + (j*3);
-
-                        //Make sure we're not checking a place
-                        //that doesn't exist
-                        if (idx >= 15) {
-                            heightWorks = false;
-                            break;
-                        }
-
-                        //Finally, check if the spot won't work
-                        if (grid[idx] == 1) {
-                            heightWorks = false;
-                            break;
-                        }
-                    }
-
-                    //Check if width is available
-                    var widthWorks = true;
-                    for (var j = 1; j <= width; j++) {
-                        //Computes the spot we're checking
-                        var idx = i + j;
-
-                        //Make sure we're not checking a place
-                        //that doesn't exist
-                        if (idx >= 15) {
-                            widthWorks = false;
-                            break;
-                        }
-
-                        //Make sure we don't go over
-                        //into the next row (wouldn't make sense)
-                        var startRow = Math.floor(i/3);
-                        var currentRow = Math.floor(idx/3);
-
-                        if (currentRow > startRow) {
-                            widthWorks = false;
-                            break;
-                        }
-
-                        //Finally, check if the spot won't work
-                        if (grid[idx] == 1) {
-                            widthWorks = false;
-                            break;
-                        }
-                    }
-
-                    //if width and height works, it's a valid spot
-                    if (widthWorkds && heightWorks) {
-                        startPosition = i;
-                        console.log(i);
-                    }
-                }
-            }
 	    });
 	});
 
@@ -99,7 +34,7 @@ function config_load() {
 			$(".tile").css("margin", "5px");
 		}
 		$(".topHeader").css("font-family",json.titleFont);
-	});	
+	});
 
 
 
